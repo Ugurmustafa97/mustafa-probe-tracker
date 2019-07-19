2ade141623beb1ffcaf30fa90204aa9b097b8f1e
Mustafa Uğur <mugur729@gmail.com>
2019-07-19 12:04:49 -0500

    Mustafa_Probe_Tracker.py file organized

--- Mustafa_Probe_Tracker/Mustafa_Probe_Tracker.py
+++ Mustafa_Probe_Tracker/Mustafa_Probe_Tracker.py
@@ -5,17 +5,8 @@ from slicer.ScriptedLoadableModule import *
 import logging
 import time
 
-#
 # Mustafa_Probe_Tracker
-#
-
-
-
-
 class Mustafa_Probe_Tracker(ScriptedLoadableModule):
-  """Uses ScriptedLoadableModule base class, available at:
-  https://github.com/Slicer/Slicer/blob/master/Base/Python/slicer/ScriptedLoadableModule.py
-  """
 
   def __init__(self, parent):
     ScriptedLoadableModule.__init__(self, parent)
@@ -24,80 +15,61 @@ class Mustafa_Probe_Tracker(ScriptedLoadableModule):
     self.parent.dependencies = []
 
 
-#
 # Mustafa_Probe_TrackerWidget
-#
-
 class Mustafa_Probe_TrackerWidget(ScriptedLoadableModuleWidget):
-  """Uses ScriptedLoadableModuleWidget base class, available at:
-  https://github.com/Slicer/Slicer/blob/master/Base/Python/slicer/ScriptedLoadableModule.py
-  """
+  """ This class is GUI buttons side of the extension. """
 
   def setup(self):
     ScriptedLoadableModuleWidget.setup(self)
     
-    # Adding new collapsible button to loading probe model and tranformation  This was editied by Mustafa UGUR 07/10/2019
+    # Adding new collapsible button to loading probe model and tranformation
     loadingCollapsibleButton = ctk.ctkCollapsibleButton()
     loadingCollapsibleButton.text = "Load Probe Model and Transformation"
     self.layout.addWidget(loadingCollapsibleButton)
 
-    
-    # Layout within the dummy collabsible button                              This was editied by Mustafa UGUR 07/10/2019
+    # Layout within the dummy collabsible button
     loadingFormLayout = qt.QFormLayout(loadingCollapsibleButton)
-    
+
+    # Create a Mustafa_Probe_TrackerLogic() class for "connect" functions for buttons
     self.logic = Mustafa_Probe_TrackerLogic()
 
-    #
-    # Load Probe Button                             This was editied by Mustafa UGUR 07/09/2019
-    #
+    # "Load Probe" Button
     self.probeButton = qt.QPushButton("Load Probe")
     self.probeButton.toolTip = "Load the Probe Model."
     self.probeButton.enabled = True
     loadingFormLayout.addRow(self.probeButton)
-    
-    #
-    # Load Transformation Button                    This was editied by Mustafa UGUR 07/09/2019
-    # 
+
+    # "Load Transformation and Apply It" Button
     self.transformationButton = qt.QPushButton("Load Transformation and Apply It")
     self.transformationButton.toolTip = "Load the Transformation."
     self.transformationButton.enabled = True
     loadingFormLayout.addRow(self.transformationButton)
-    
-    
-    
-    #
-    # Start the marker tracker                      
-    #
+
+    # "Start the camera" Button
     self.markerTrackerButton = qt.QPushButton("Start the camera")
     self.markerTrackerButton.toolTip = "Starts the camera"
     self.markerTrackerButton.enabled = True
     loadingFormLayout.addRow(self.markerTrackerButton)
-    
-    #
-    # Load the 3D Model
-    #
+
+    # "Load the 3D Model" Button
     self.load3DModelButton = qt.QPushButton("Load the 3D Model")
     self.load3DModelButton.toolTip = "Loads the 3D Model and Fudicial points"
     self.load3DModelButton.enabled = True
     loadingFormLayout.addRow(self.load3DModelButton)
-    
-    #
+
     # Do hierarchy setting again
-    #
     #self.setHierarchyButton = qt.QPushButton("Set the hierarchy")
     #self.setHierarchyButton.toolTip = "Sets the hieararchy again for tracking"
     #self.setHierarchyButton.enabled = True
     #loadingFormLayout.addRow(self.setHierarchyButton)
-    
-    #
-    # Stop the camera
-    #
+
+    # "Stop the camera" Button
     self.stopTrackerButton = qt.QPushButton("Stop the camera")
     self.stopTrackerButton.toolTip = "Stops the camera"
     self.stopTrackerButton.enabled = True
     loadingFormLayout.addRow(self.stopTrackerButton)
     
-    # connections
+    # Connections for Buttons
     self.probeButton.connect('clicked(bool)',self.logic.loadProbeModel)
     self.transformationButton.connect('clicked(bool)', self.logic.loadPivotTransform)
     self.markerTrackerButton.connect('clicked(bool)', self.logic.startCamera)
@@ -108,49 +80,36 @@ class Mustafa_Probe_TrackerWidget(ScriptedLoadableModuleWidget):
     # Add vertical spacer
     self.layout.addStretch(1)
 
-  
-  
-  
-    
-#
-# Mustafa_Probe_TrackerLogic
-#
 
+# Mustafa_Probe_TrackerLogic
 class Mustafa_Probe_TrackerLogic(ScriptedLoadableModuleLogic):
-  """This class should implement all the actual
-  computation done by your module.  The interface
-  should be such that other python code can import
-  this class and make use of the functionality without
-  requiring an instance of the Widget.
-  Uses ScriptedLoadableModuleLogic base class, available at:
-  https://github.com/Slicer/Slicer/blob/master/Base/Python/slicer/ScriptedLoadableModule.py
-  """
-  
+
   def __init__(self):
     self.trackerNode = None
     #self.markerModel = None
-  
-  #This was editied by Mustafa UGUR 07/09/2019
+
+  # This function loads the Needle Model that we created previously.
   def loadProbeModel(self):
     slicer.util.loadModel("C:\Users\Mustafa Ugur\Documents\NeedleModel.vtk")  
-  
+
+  # This function loads the transformation from the pivot calibration that we
+  # done previously sets the hierarcy between needle and transformation.
   def loadPivotTransform(self):
     slicer.util.loadTransform("C:\Users\Mustafa Ugur\Documents\NeedleTipToMarker.h5")
     needleModel = slicer.util.getNode('NeedleModel')
     transformNode = slicer.util.getNode('NeedleTipToMarker')
     needleModel.SetAndObserveTransformNodeID(transformNode.GetID())
   
-  # This fuction starts camera for tracking 
+  # This fuction starts camera for tracking.
   def startCamera(self):
-    # Starting the camera
+    # Starting the camera and create a node for camera.
     if not self.trackerNode:
       self.trackerNode = slicer.vtkMRMLIGTLConnectorNode()
       slicer.mrmlScene.AddNode(self.trackerNode)
       self.trackerNode.SetName('markerTracker')  
     
     self.trackerNode.Start()
-    
-    
+
     try:
       transformNode = slicer.util.getNode('NeedleTipToMarker')
       markerModel = slicer.util.getNode('Marker4ToTracker')
@@ -158,8 +117,6 @@ class Mustafa_Probe_TrackerLogic(ScriptedLoadableModuleLogic):
     except:
       print("Marker4ToTracker Exepcion occured, please show the chechkerboard to camera and press Start the Camera Button Again")
     
-    
-  
   """
   def setHierarcy(self): 
     # Setting the hierarchy again
@@ -167,11 +124,11 @@ class Mustafa_Probe_TrackerLogic(ScriptedLoadableModuleLogic):
     markerModel = slicer.util.getNode('Marker4ToTracker')
     transformNode.SetAndObserveTransformNodeID(markerModel.GetID())
   """
+  # This function stops the camera.
   def stopCamera(self):
     self.trackerNode.Stop()
-    
+
+  # This function loads the 3D file was created for surface tracking.
   def load3DModel(self):
     slicer.util.loadModel("C:\Users\Mustafa Ugur\Desktop\simple_box\simple_box.STL")
     slicer.util.loadMarkupsFiducialList("C:\Users\Mustafa Ugur\Documents\From.fcsv")
-
-
